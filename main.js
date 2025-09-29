6be3d201822907ba42dd9e2dd3c24c5a8df6c987
Morten Stig Christensen <github@mschristensen.com>
2025-09-29 10:24:24 +0900

    Update main.js

--- main.js
+++ main.js
@@ -49,20 +49,20 @@
   const CFG = {
     first: {
       visual: { start: "top 85%",   end: "bottom top", dist: -320, blur: 6 },
-      title:  { start: "top 55%",   end: "bottom top", dist: 360 },
-      block:  { start: "bottom 115%", end: "bottom top", dist: -240 },
+      title:  { start: "top 55%",   end: "bottom top", dist: 320 },
+      block:  { start: "bottom 115%", end: "bottom top", dist: -320 },
       contrastEnabled: true
     },
     middle: {
       visual: { start: "top 85%",   end: "bottom top", dist: -320, blur: 6 },
-      title:  { start: "top 70%",   end: "bottom top", dist: 360 },
-      block:  { start: "bottom 115%", end: "bottom top", dist: -240 },
+      title:  { start: "top 70%",   end: "bottom top", dist: 320 },
+      block:  { start: "bottom 115%", end: "bottom top", dist: -320 },
       contrastEnabled: true
     },
     last: {
       visual: { start: "top 85%",   end: "bottom top", dist: -320, blur: 6 },
-      title:  { start: "top 70%",   end: "bottom top", dist: 360 },
-      block:  { start: "bottom 115%", end: "bottom top", dist: -200 },
+      title:  { start: "top 70%",   end: "bottom top", dist: 320 },
+      block:  { start: "bottom 100%", end: "bottom top", dist: -200 },
       contrastEnabled: false
     }
   };
